11763209647a0a82907654e48bd8a19a28fdc170
José Luis Valdiviezo Peña <luisj.valdiviezo20@gmail.com>
2023-12-21 14:47:49 -0500

    registro

--- src/pages/registrarme.js
+++ src/pages/registrarme.js
@@ -23,6 +23,7 @@ export default function Page() {
   };
 
   const onSubmit = async (ev) => {
+    if (nombres == "" || apellidos == "" || email == "" || telefono == "" || clave == "") return;
     const response = await fetch("https://lofar-api-uskfbty6la-ue.a.run.app/registrarse", {
       method: "POST",
       headers: {
